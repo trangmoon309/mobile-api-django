fe71f7b6262c1811b10c68485efb54a72e5fabc8
lioaslan <leoalan5577@gmail.com>
2021-07-15 00:08:57 +0700

    Remove unused class

--- mobile_project/mobile_api/urls.py
+++ mobile_project/mobile_api/urls.py
@@ -1,41 +1,37 @@
-from django.urls import path, include
-from . import views
+from django.urls import path
 #from .views import UploadFileViewSet
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-
 
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 
-from rest_framework.routers import DefaultRouter
+from . import views
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="API for mobile final",
-      default_version='v1',
-      description="Document for API",
-      terms_of_service="https://www.pbl5.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+SchemaView = get_schema_view(
+    openapi.Info(
+        title="API for mobile final",
+        default_version='v1',
+        description="Document for API",
+        terms_of_service="https://www.pbl5.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny, ),
 )
 
-
 urlpatterns = [
-   path('users', views.UserAPIView.as_view()),   
-   path('users/sign-up', views.SignUpAPIView.as_view()),   
-   path('users/log-in', views.LoginAPIView.as_view()),
-   path('categories', views.CategoryAPIView.as_view()),
-   path('foods/', views.FoodAPIView.as_view()), 
-   path('foods/by-ingredient', views.FoodByIngredientAPIView.as_view()),     
-   path('reviews/', views.ReviewAPIView.as_view()),
-   path('ingredients/', views.IngredientAPIView.as_view()),
-   #path('food-ingredients/', views.FoodIngredientAPIView.as_view()),
-   path('user-fav-foods/', views.UserFavoriteFoodAPIView.as_view()),
-   path('doc', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('users', views.UserAPIView.as_view()),
+    path('users/sign-up', views.SignUpAPIView.as_view()),
+    path('users/log-in', views.LoginAPIView.as_view()),
+    path('categories', views.CategoryAPIView.as_view()),
+    path('ingredients/', views.IngredientAPIView.as_view()),
+    path('doc',
+         SchemaView.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/',
+         SchemaView.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
 ]
--- mobile_project/mobile_api/views.py
+++ mobile_project/mobile_api/views.py
@@ -1,23 +1,16 @@
-from django.http import JsonResponse, HttpResponse, response
+import uuid
+
+from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 
-from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
-from rest_framework import generics, viewsets
-from rest_framework.parsers import MultiPartParser, FormParser, JSONParser, FileUploadParser
-from rest_framework.decorators import parser_classes
-
-from .models import User, Food, UserFavoriteFood, Ingredient, FoodIngredient, Review, Category
-from . import serializers
-from rest_framework.views import APIView
+from rest_framework import generics
 
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
-import uuid
-
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from .models import User, Food, UserFavoriteFood, Ingredient, FoodIngredient, Review, Category
+from . import serializers
 
 
 class UserAPIView(generics.GenericAPIView):
@@ -109,229 +102,10 @@ class CategoryAPIView(generics.GenericAPIView):
         serializer = serializers.CategorySerializer(categories, many=True)
         return Response(serializer.data)
 
-    def post(self, request, *args, **kwargs):
-        category_data = request.data
-        id = uuid.uuid4()
-        new_category = Category.objects.create(id=id,
-                                               name=category_data["name"])
-        new_category.save()
-        serializer = serializers.CategorySerializer(new_category)
-        return Response(serializer.data)
-
-    id_param = openapi.Parameter('id',
-                                 in_=openapi.IN_QUERY,
-                                 type=openapi.TYPE_STRING)
-
-    @swagger_auto_schema(manual_parameters=[id_param])
-    def delete(self, request, *args, **kwargs):
-        Category.objects.filter(id=request.query_params["id"]).delete()
-        return Response("Success!")
-
-
-class FoodAPIView(generics.GenericAPIView):
-    serializer_class = serializers.FoodRequestSerializer
-    throttle_scope = "foods_app"
-
-    def post(self, request, *args, **kwargs):
-        food_data = request.data
-        id = uuid.uuid4()
-
-        # Lay phan loai
-        category = Category.objects.get(id=food_data["category_id"])
-
-        # Tao thuc an moi
-        new_food = Food.objects.create(id=id,
-                                       name=food_data["name"],
-                                       clorie=food_data["clorie"],
-                                       potion=food_data["potion"],
-                                       level=food_data["level"],
-                                       star_level=food_data["star_level"],
-                                       prepare=food_data["prepare"],
-                                       youtube_url=food_data["youtube_url"],
-                                       category=category)
-        new_food.save()
-
-        # Tao bang trung gian food-ingredient
-        ingredients = food_data["ingredients"]
-        ingredient_responses = []
-        for ingredient in ingredients:
-            added_ingredient = Ingredient.objects.get(
-                id=ingredient['ingredient_id'])
-            new_food_ingredient = FoodIngredient.objects.create(
-                id=id,
-                food=new_food,
-                ingredient=added_ingredient,
-                quantity=ingredient['quantity'])
-            new_food_ingredient.save()
-            ingredient_serializer = serializers.FoodIngredientResponseSerializer(
-                new_food_ingredient)
-            ingredient_responses.append(ingredient_serializer.data)
-
-        serializer = serializers.FoodResponseSerializer(new_food)
-        response = serializer.data
-        response['ingredients'] = ingredient_responses
-        return Response(response)
-
-    def get(self, request):
-        foods = Food.objects.all()
-        response = []
-        for food in foods:
-            ingredient_responses = []
-            food_ingredients = FoodIngredient.objects.filter(food=food)
-            for ingredient in food_ingredients:
-                ingredient_serializer = serializers.FoodIngredientResponseSerializer(
-                    ingredient)
-                ingredient_responses.append(ingredient_serializer.data)
-            food_serializer = serializers.FoodResponseSerializer(food)
-            response_item = food_serializer.data
-            response_item['ingredients'] = ingredient_responses
-            response.append(response_item)
-
-        return Response(response)
-
-    id_param = openapi.Parameter('id',
-                                 in_=openapi.IN_QUERY,
-                                 type=openapi.TYPE_STRING)
-
-    @swagger_auto_schema(manual_parameters=[id_param])
-    def delete(self, request, *args, **kwargs):
-        Food.objects.filter(id=request.query_params["id"]).delete()
-        return Response("Success!")
-
-
-class ReviewAPIView(generics.GenericAPIView):
-    serializer_class = serializers.ReviewRequestSerializer
-    throttle_scope = "reviews_app"
-
-    def post(self, request, *args, **kwargs):
-        review_data = request.data
-        id = uuid.uuid4()
-        food = Food.objects.get(id=review_data["food_id"])
-        new_review = Review.objects.create(id=id,
-                                           content=review_data["content"],
-                                           food=food)
-        new_review.save()
-        serializer = serializers.ReviewResponseSerializer(new_review)
-        return Response(serializer.data)
-
-    def get(self, request):
-        reviews = Review.objects.all()
-        serializer = serializers.ReviewResponseSerializer(reviews, many=True)
-        return Response(serializer.data)
-
-    id_param = openapi.Parameter('id',
-                                 in_=openapi.IN_QUERY,
-                                 type=openapi.TYPE_STRING)
-
-    @swagger_auto_schema(manual_parameters=[id_param])
-    def delete(self, request, *args, **kwargs):
-        Review.objects.filter(id=request.query_params["id"]).delete()
-        return Response("Success!")
-
-
-class UserFavoriteFoodAPIView(generics.GenericAPIView):
-    serializer_class = serializers.UserFavoriteFoodRequestSerializer
-    throttle_scope = "favorites_food_app"
-
-    def post(self, request, *args, **kwargs):
-        data = request.data
-        id = uuid.uuid4()
-        food = Food.objects.get(id=data["food_id"])
-        user = User.objects.get(id=data["user_id"])
-        new_fav = UserFavoriteFood.objects.create(id=id, food=food, user=user)
-        new_fav.save()
-        serializer = serializers.UserFavoriteFoodResponseSerializer(new_fav)
-        return Response(serializer.data)
-
-    def get(self, request):
-        fav_foods = UserFavoriteFood.objects.all()
-        serializer = serializers.UserFavoriteFoodResponseSerializer(fav_foods,
-                                                                    many=True)
-        return Response(serializer.data)
-
-    id_param = openapi.Parameter('id',
-                                 in_=openapi.IN_QUERY,
-                                 type=openapi.TYPE_STRING)
-
-    @swagger_auto_schema(manual_parameters=[id_param])
-    def delete(self, request, *args, **kwargs):
-        UserFavoriteFood.objects.filter(id=request.query_params["id"]).delete()
-        return Response("Success!")
-
 
 class IngredientAPIView(generics.GenericAPIView):
-    serializer_class = serializers.IngredientSerializer
-    throttle_scope = "ingredients_app"
-
-    def post(self, request, *args, **kwargs):
-        data = request.data
-        id = uuid.uuid4()
-        new_ingredient = Ingredient.objects.create(id=id, name=data["name"])
-        new_ingredient.save()
-        serializer = serializers.IngredientSerializer(new_ingredient)
-        return Response(serializer.data)
-
     @swagger_auto_schema(operation_description='Get all ingredients')
     def get(self, request):
         ingredients = Ingredient.objects.all()
         serializer = serializers.IngredientSerializer(ingredients, many=True)
         return Response(serializer.data)
-
-    id_param = openapi.Parameter('id',
-                                 in_=openapi.IN_QUERY,
-                                 type=openapi.TYPE_STRING)
-
-    @swagger_auto_schema(manual_parameters=[id_param])
-    def delete(self, request, *args, **kwargs):
-        Ingredient.objects.filter(id=request.query_params["id"]).delete()
-        return Response("Success!")
-
-
-class FoodByIngredientAPIView(generics.GenericAPIView):
-    ingredient_id_param = openapi.Parameter('ingredient_id',
-                                            in_=openapi.IN_QUERY,
-                                            type=openapi.TYPE_STRING)
-    throttle_scope = "foods_app"
-
-    @swagger_auto_schema(
-        manual_parameters=[ingredient_id_param],
-        operation_summary="Get danh sách món ăn dựa vào nguyên liệu")
-    def get(self, request):
-        foods = Food.objects.all()
-        food_ingredients = FoodIngredient.objects.all()
-        food_ids = []
-        responses = []
-        ingredient_id = request.query_params["ingredient_id"]
-        for item in food_ingredients:
-            if str(item.ingredient_id) == ingredient_id:
-                food_ids.append(str(item.food_id))
-
-        for food in foods:
-            ingredient_responses = []
-            food_ingredients = FoodIngredient.objects.filter(food=food)
-            for ingredient in food_ingredients:
-                ingredient_serializer = serializers.FoodIngredientResponseSerializer(
-                    ingredient)
-                ingredient_responses.append(ingredient_serializer.data)
-            food_serializer = serializers.FoodResponseSerializer(food)
-            response_item = food_serializer.data
-            response_item['ingredients'] = ingredient_responses
-            responses.append(response_item)
-
-        return Response(responses)
-
-
-# Bang trung gian
-
-# class FoodIngredientAPIView(generics.GenericAPIView):
-#     serializer_class = serializers.FoodIngredientRequestSerializer
-#     throttle_scope = "food_ingredient_app"
-#     def post(self, request, *args, **kwargs):
-#         data = request.data
-#         id = uuid.uuid4()
-#         food = Food.objects.get(id = data["food_id"])
-#         ingredient = Ingredient.objects.get(id = data["ingredient_id"])
-#         new_food_ingredient = FoodIngredient.objects.create(id=id, food = food, ingredient = ingredient, quantity = data["quantity"])
-#         new_food_ingredient.save()
-#         serializer = serializers.FoodIngredientResponseSerializer(new_food_ingredient)
-#         return Response(serializer.data)
